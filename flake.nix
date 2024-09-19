d64f49be5454f0502b1114511dca26dbe62f4ab1
Chris LaRose <cjlarose@gmail.com>
2024-09-18 20:02:25 -0700

    Add nix flake

---
+++ flake.nix
@@ -0,0 +1,27 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-24.05";
+  }; 
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "x86_64-darwin" "aarch64-linux" "aarch64-darwin" ];
+    in {
+      devShells = nixpkgs.lib.genAttrs supportedSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.mkShell {
+            buildInputs = with pkgs; [
+              ghostscript
+              netpbm
+              perl
+              texliveFull
+              # texlive.combined.scheme-basic # doesn't include preview.sty
+              # (texliveBasic.withPackages (ps: with ps; [ preview ])) # can't find preview.sty still
+              # texlive.combined.scheme-medium # fails on darwin
+            ];
+          };
+        }
+      );
+    };
+}
