ec7aeb389f45e8d3b2af5964d4f8fbfecb935978
Chris LaRose <cjlarose@gmail.com>
2024-09-19 03:59:59 +0000

    Add default package to flake outputs

--- flake.nix
+++ flake.nix
@@ -21,5 +21,35 @@
           };
         }
       );
+
+      packages = nixpkgs.lib.genAttrs supportedSystems (system:
+        let pkgs = nixpkgs.legacyPackages.${system};
+        in {
+          default = pkgs.stdenv.mkDerivation {
+            pname = "latex2html";
+            version = "2024";
+            src = ./.;
+            buildInputs = with pkgs; [
+              ghostscript
+              netpbm
+              perl
+              poppler_utils
+              texliveFull
+            ];
+            nativeBuildInputs = [ pkgs.makeWrapper ];
+            configurePhase = ''
+              ./configure \
+                --prefix="$out" \
+                --without-mktexlsr \
+                --with-texpath=$out/share/texmf/tex/latex/html
+            '';
+            postInstall = ''
+              for p in $out/bin/{latex2html,pstoimg}; do \
+                wrapProgram $p --add-flags '--tmp="''${TMPDIR:-/tmp}"'
+              done
+            '';
+          };
+        }
+      );
     };
 }
